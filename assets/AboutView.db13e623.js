d147433b4f6adb5b97b8308629967e95aceca296
connectshark <46649021+connectshark@users.noreply.github.com>
2021-12-02 05:47:47 +0000

    Deploying to gh-pages from @ connectshark/vite-todo@ab610ab880ea69cf320510ce84ab9c6c63a0567a 🚀

--- assets/AboutView.db13e623.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.a989b69b.js";import{o,c as t,a as s}from"./vendor.64f86ceb.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(i,d){return o(),t("div",a,n)}var f=e(_,[["render",r]]);export{f as default};
