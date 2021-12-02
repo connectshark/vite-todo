cd4b5fd5fbd1e768489a8d9c472f2c3673bcf741
connectshark <bobhus394@gmail.com>
2021-12-02 15:02:26 +0800

    修改預設內容

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+</script>
+
+<template>
+<main class="home">
+  <h2>{{counterStore.doubleCount}}</h2>
+  <input type="button" value="+" @click="counterStore.increment">
+</main>
+</template>
+
+<style scoped>
+
+</style>
