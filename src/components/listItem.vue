8239ece33969f894b41b0c6954493ff51429e48a
connectshark <bobhus394@gmail.com>
2021-12-10 18:07:14 +0800

    新增畫線樣式

--- src/components/listItem.vue
+++ src/components/listItem.vue
@@ -28,10 +28,10 @@ export default {
 
 <template>
 <li class="mx-auto w-4/5 bg-white rounded-2xl p-4 mb-4 transition hover:shadow-lg shadow-slate-400">
-  <p class="text-gray-700 text-2xl inline-block">
-    <span class="text-xl cursor-pointer" @click="modify">
-      <i class='bx bx-check-circle text-indigo-700' v-if="checked"></i>
-      <i class='bx bx-circle text-indigo-300' v-else></i>
-    </span>{{content}}</p>
+  <div class="text-xl cursor-pointer inline-block align-middle" @click="modify">
+    <i class='bx bx-check-circle text-indigo-700' v-if="checked"></i>
+    <i class='bx bx-circle text-indigo-300' v-else></i>
+  </div>
+  <p class="text-gray-700 text-2xl inline-block decoration-indigo-700" :class="{ 'line-through' : checked}">{{content}}</p>
 </li>
 </template>
